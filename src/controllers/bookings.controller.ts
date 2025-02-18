33a13e43a2a8cbd8db2a4c8f27b5512017a9a2b5
patiencemanzen <hseal419@gmail.com>
2025-02-18 09:55:11 +0200

    refactor booking management by introducing service layer for handling bookings and updates

--- src/controllers/bookings.controller.ts
+++ src/controllers/bookings.controller.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import { tryCatch } from '../utils/trycatch';
 import { IUser } from '../types/user.types';
-import { createBooking, getAllBookings, getBookingById, updateBooking as updateBookingModel, deleteBooking as deleteBookingModel } from '../models/booking.model';
+import { getAllBookings, getBookingById, deleteBooking as deleteBookingModel } from '../models/booking.model';
 import { bookingSchema } from '../validations/booking.validation';
+import { handleBooking, handleUpdateBooking } from '../services/bookings.service';
 
 interface CustomRequest extends Request {
     user: IUser;
@@ -10,7 +11,7 @@ interface CustomRequest extends Request {
 
 /**
  * List all User Booking
- * 
+ *      
  * @param req Request
  * @param res Response
  */
@@ -63,7 +64,8 @@ export const storeBookings = async (req: CustomRequest, res: Response) => {
         if (!user)
             return res.status(401).json({ success: false, message: 'Unauthorized' });
 
-        const booking = await createBooking(user.id, {
+        const booking = await handleBooking({
+            renterId: user.id,
             propertyId: validatedData.propertyId,
             checkIn: new Date(validatedData.checkIn),
             checkOut: new Date(validatedData.checkOut),
@@ -71,6 +73,8 @@ export const storeBookings = async (req: CustomRequest, res: Response) => {
         });
 
         return res.status(201).json({ success: true, booking });
+    }, (error) => {
+        return res.status(400).json({ success: false, message: error.message });
     });
 }
 
@@ -89,7 +93,8 @@ export const updateBookings = async (req: CustomRequest, res: Response) => {
         if (!user)
             return res.status(401).json({ success: false, message: 'Unauthorized' });
 
-        const booking = await updateBookingModel(id, {
+        const booking = await handleUpdateBooking({
+            id: id,
             propertyId: validatedData.propertyId,
             checkIn: new Date(validatedData.checkIn),
             checkOut: new Date(validatedData.checkOut),
@@ -101,6 +106,8 @@ export const updateBookings = async (req: CustomRequest, res: Response) => {
             return res.status(404).json({ success: false, message: 'Booking not found' });
 
         return res.status(200).json({ success: true, booking });
+    }, (error) => {
+        return res.status(400).json({ success: false, message: error.message });
     });
 }
 
